6582ff3f970a7c0c8bd2de31312086c074e046fe
Malhaar16 <malhaarpatel16101998@gmail.com>
2024-09-16 17:30:26 -0400

    Day 2

--- ReactJS/react-app-basics/src/App.tsx
+++ ReactJS/react-app-basics/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   //const handleSelect = (item:string)=> {
     //console.log(item);
   
-  //D2 --> Execerise:1
+  //D2 --> Concept
   //Learning how to share a state hook with two component
   //--> Using E-Comenrece website where user add the product to the cart and also get updated to navbar --> Take a look at walmart or amazon website for reference
   const [cartItem, setCartItem] = useState(['Product1', 'Product2']);
@@ -25,6 +25,52 @@ function App() {
     const newItem = "Product"+(cartItem.length+1)
     setCartItem([...cartItem, newItem]);
   }
+
+
+  //D2 --> Exercise: 1
+  //To change the name for the player in the nested object state on click.
+  // const [game, setGame] = useState({
+  //   id: 1,
+  //   player: {
+  //     name: "John",
+  //   },
+  // });
+
+  // const handleClick = () => {
+  //   setGame({...game, player: {...game.player, name: "Bob"}})
+
+  // }
+
+  //D2 --> Exercise: 2
+  //Add the toppings to a array property in the object state
+  // const [pizza, setPizza] = useState({
+  //   name:"Paneer",
+  //   toppings: ['Hot peppers']
+  // });
+
+  // const handleClick = () => {
+  //   setPizza({...pizza, toppings:[...pizza.toppings, "onions"]})
+
+  // }
+
+  //D2 --> Exercise: 3
+  //In this exercide whenever button is clicked quantity of product 1 should change to 2
+
+  // const [cart, setCart] = useState({
+  //   discount: 0.1,
+  //   items: [
+  //     {id:1, title: 'Product1', quantity:1},
+  //     {id:2, title: 'Product2', quantity:2}
+  //   ]
+  // })
+
+  // const handleClick = () => {
+  //   setCart({...cart, items: [...cart.items, {id:1, title:"Product1", quantity:2}]})
+  // }
+
+  // const handleClick = () => {
+  //   setCart({...cart, items: cart.items.map(item => item.id === 1 ? {...item , quantity:2}: item)})
+  // }
 
   return (
     <div>
@@ -41,3 +87,4 @@ function App() {
 }
 
 export default App;
+
