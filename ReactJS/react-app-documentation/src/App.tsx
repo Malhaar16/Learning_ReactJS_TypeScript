b3083a9a44a5ccb060da730556dc4b9f88e579e3
Malhaar16 <malhaarpatel16101998@gmail.com>
2024-09-20 18:30:13 -0400

    Day 4 Following React Documentation

---
+++ ReactJS/react-app-documentation/src/App.tsx
@@ -0,0 +1,16 @@
+import Picture from "./components/Picture";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        React LOGO
+      </h1>
+      <Picture />
+      <Picture />
+      <Picture />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/react-app-documentation/src/components/Picture.tsx
@@ -0,0 +1,19 @@
+const Picture = () => {
+    return <img src="https://i.imgur.com/Yuu4pFD.jpeg`" alt="Welcome to React World"></img>
+}
+
+export default Picture
+
+
+/**
+ * You’ve just gotten your first taste of React! Let’s recap some key points.
+
+React lets you create components, reusable UI elements for your app.
+
+In a React app, every piece of UI is a component.
+
+React components are regular JavaScript functions except:
+
+Their names always begin with a capital letter.
+They return JSX markup.
+ */
